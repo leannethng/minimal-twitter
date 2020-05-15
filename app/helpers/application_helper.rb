9c8a75aa8ec2d5dcda828ac526fcd28a3a63b2fb
leannethng <leanne.thng@gmail.com>
2020-05-15 11:11:12 -0700

    disable images for mobile and tablet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Twitter::TwitterText::Autolink
+
+  def mobile_device
+    agent = request.user_agent
+    return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
+    return "mobile" if agent =~ /Mobile/
+    return "desktop"
+  end
+
 end
